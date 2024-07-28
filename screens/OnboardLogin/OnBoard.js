a5213fcfbd6178f4e4bc50665e8b3ace1d73b95e
Debadyuti Mukherjee <debadyuti1010@gmail.com>
2024-07-28 19:41:44 +0530

    onboard page update
    greeting (now onboard) page frontend update

---
+++ screens/OnboardLogin/OnBoard.js
@@ -0,0 +1,58 @@
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import Login from './Login'
+
+export default function OnBoard() {
+
+  const navigation = useNavigation()
+
+  function toLogin () {
+    navigation.navigate("Login")
+  }
+
+  return (
+    <ImageBackground source={require("../../assets/images/onBoard.webp")} resizeMode='cover'
+      style={styles.imageBody}>
+      <View style={styles.contentArea}>
+        <Text style={[styles.text, {fontSize: 24, fontWeight: '300'}]}>Let's work together</Text>
+        <Text style={[styles.text, {fontSize: 18, fontWeight: '400', textAlign: 'center'}]}>Stay on top of Uni life and make every moment count.</Text>
+        <TouchableOpacity style={styles.positiveBtn} onPress={toLogin}>
+          <Text style={[styles.text, {fontSize: 24, fontWeight: '400' }]}>Let's Start</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+  imageBody: {
+    height: '100%',
+    width: '100%',
+    alignItems: 'center'
+  },
+  contentArea: {
+    position: 'absolute',
+    backgroundColor: '#000',
+    opacity: 0.95,
+    bottom: 0,
+    width: '100%',
+    alignItems: 'center',
+    rowGap: 24,
+    paddingHorizontal: 16,
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    paddingVertical: 36,
+  },
+  positiveBtn: {
+    backgroundColor: '#379777',
+    width: '100%',
+    alignItems: 'center',
+    borderRadius: 25,
+    paddingVertical: 6,
+  },
+  text: {
+    fontFamily: 'Outfit',
+    color: '#FFF'
+  }
+})
