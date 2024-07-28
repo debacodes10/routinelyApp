a913cc52148b485048725f71de4fe11ebc41a0fc
Debadyuti Mukherjee <debadyuti1010@gmail.com>
2024-07-28 22:40:01 +0530

    onboarding and login update
    created frontend for onboard page and login and register pages

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnBoard from "./screens/OnboardLogin/OnBoard";
+import Register from "./screens/OnboardLogin/Register";
 import Login from "./screens/OnboardLogin/Login";
 
 const Stack = createNativeStackNavigator();
@@ -12,6 +13,10 @@ export default function App () {
         <Stack.Screen
         component={OnBoard}
         name="OnBoard"
+        options={{headerShown: false}} />
+        <Stack.Screen
+        component={Register}
+        name="Register"
         options={{headerShown: false}} />
         <Stack.Screen
         component={Login}
